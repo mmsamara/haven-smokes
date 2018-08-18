a98026ff4f9a2a0aae32fc939a04832e7d79b47f
mmsamara <mmsamara@ncsu.edu>
2018-08-18 15:06:11 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="jumbotron">
+          <h1 className="text-center">Haven Smokes</h1> 
+          <p className="text-center">Click to find out more about any of our products</p> 
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
